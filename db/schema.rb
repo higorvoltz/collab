5390327ef3ee87bcee35664573947111c3250a0c
higorvoltz <higor.amorim@voltzmotors.com>
2023-06-23 18:14:43 -0300

    feat: add name field on users table, include name attribute to sign_up params and account_update_params methods the RegistrationsController

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2023_06_23_204806) do
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
