9e46365b7a1d0290591520e3b0c1b25a46ac364b
higorvoltz <higor.amorim@voltzmotors.com>
2023-06-24 16:40:27 -0300

    feat: add sign_up spec

---
+++ spec/features/user/sign_up_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Sign up', type: :feature do
+  let(:user) { build(:user) }
+
+  it 'user navigates to sign up page and successfully signs up',
+     js: true do
+    visit root_path
+    find('nav a', text: 'Signup').click
+    fill_in 'user[name]', with: user.name
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    fill_in 'user[password_confirmation]', with: user.password_confirmation
+    find('.sign-up-button').click
+    expect(page).to have_selector('#user-settings')
+  end
+end
